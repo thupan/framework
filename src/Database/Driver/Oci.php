035026dfbe3961760f2a72fe73aaeb7c508e1f44
Luiz Schmitt <lzschmitt@gmail.com>
2017-03-08 13:38:51 -0400

    correcao do metodo callprocedure

--- src/Database/Driver/Oci.php
+++ src/Database/Driver/Oci.php
@@ -94,7 +94,6 @@ class Oci extends \PDO implements \Database\Interfaces\PersistenceDatabase
             $sth = $connection->prepare("CALL $sp_name($args)");
 
             for($i = 0, $z =1; $i < count($sp_args); $i++, $z++) {
-                dd($sp_args[$i]);
                 $sth->bindParam($z, $sp_args[$i], \PDO::PARAM_STR|\PDO::PARAM_INPUT_OUTPUT, 2000);
             }
 
