e9f266d8c4d1c8b0ad3dc20b15a7de83fccccb75
David Borges <david.borges@pmm.am.gov.br>
2018-10-30 09:45:44 -0400

    removido magic quotes do pdf

--- src/Service/PDF.php
+++ src/Service/PDF.php
@@ -617,7 +617,7 @@ function Image($file, $x = null, $y = null, $w=0, $h=0, $type='', $link='',  $is
         }
         $type=strtolower($type);
         $mqr=get_magic_quotes_runtime();
-        set_magic_quotes_runtime(0);
+        //set_magic_quotes_runtime(0);
         if($type=='jpg' || $type=='jpeg')
             $info=$this->_parsejpg($file);
         elseif($type=='png'){
@@ -632,7 +632,7 @@ function Image($file, $x = null, $y = null, $w=0, $h=0, $type='', $link='',  $is
                 $this->Error('Unsupported image type: '.$type);
             $info=$this->$mtd($file);
         }
-        set_magic_quotes_runtime($mqr);
+        //set_magic_quotes_runtime($mqr);
         
         if ($isMask){
       $info['cs']="DeviceGray"; // try to force grayscale (instead of indexed)
